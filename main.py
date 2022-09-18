6cf80d44161920c3a9fc782e3db6a07ca95c804e
DTjomsland <dtjomslandjr@gmail.com>
2022-09-18 20:40:16 +0800

    User model created

--- main.py
+++ main.py
@@ -9,11 +9,37 @@ ma = Marshmallow(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://david:Cygyxy64rtfg@localhost:5432/sprout_planner"
 
+
+# User table creation
 class Users(db.Model):
         __tablename__="users"
 
-        id = db.Column(db.Integer, primary_key=True)
+        user_id = db.Column(db.Integer, primary_key=True)
+        user_email = db.Column(db.String, unique=True, nullable=False)
+        user_password = db.Column(db.Text(60), nullable=False)
+
+
+@app.cli.command('create')
+def create_db():
+    db.create_all()
+    print("Creating tables...")
+
+
+@app.cli.command('seed')
+def seed_db():
+    users = Users(
+        user_email="user@gmail.com",
+        user_password="password"
+    )
+
+    db.session.add(users)
+
+    db.session.commit()
+    print("Seeding tables...")
 
 @app.route('/')
 def welcome():
     return "Sprout Planner under construction"
+
+if __name__ == '__main__':
+    app.run(debug=True)
