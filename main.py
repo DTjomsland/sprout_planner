272a69cc0e16b74d2ad8eafacf489d7b4b6edcc7
DTjomsland <dtjomslandjr@gmail.com>
2022-09-19 13:01:59 +0800

    Added Admin to DB

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from marshmallow.validate import Length
 import psycopg2
 
 app = Flask(__name__)
@@ -11,24 +12,34 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-class CardSchema(ma.Schema):
+class UserSchema(ma.Schema):
     class Meta:
-        fields = ('user_id', 'user_email', 'user_password')
+        fields = ('user_id', 'user_email', 'user_password', 'admin')
+    password = ma.String(validate=Length(min=8))
 
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
 
 # User table creation
 class Users(db.Model):
-        __tablename__="users"
+    __tablename__="users"
+    
+    # User columns
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_email = db.Column(db.String, unique=True, nullable=False)
+    user_password = db.Column(db.String(60), nullable=False)
+    admin = db.Column(db.Boolean, default=False)
 
-        user_id = db.Column(db.Integer, primary_key=True)
-        user_email = db.Column(db.String, unique=True, nullable=False)
-        user_password = db.Column(db.Text(60), nullable=False)
-
-
+#Create the database tables
 @app.cli.command('create')
 def create_db():
     db.create_all()
     print("Creating tables...")
+
+@app.cli.command('drop')
+def drop_db():
+    db.drop_all()
+    print("Dropped tables...")
 
 # Seeding the database
 @app.cli.command('seed')
@@ -40,6 +51,14 @@ def seed_db():
 
     db.session.add(users)
 
+    admin = Users(
+        user_email = "admin@gmail.com",
+        user_password = "admin",
+        admin = True
+    )
+
+    db.session.add(admin)
+
     db.session.commit()
     print("Seeding tables...")
 
@@ -50,9 +69,9 @@ def welcome():
 @app.route('/users')
 def users():
     users_list = Users.query.all()
-    result = CardSchema(many=True).dump(users_list)
+    result = users_schema.dump(users_list)
     return jsonify(result)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
