b4c01ddc743ea9c085633f4ea140c67370c32e66
DTjomsland <dtjomslandjr@gmail.com>
2022-09-30 21:05:34 +0800

    Began adding cloudinary function

--- controllers/user_icon_controller.py
+++ controllers/user_icon_controller.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, jsonify, request, abort
-from main import db, bcrypt, ma
+import os
+from flask import Blueprint, jsonify, request, Flask, render_template
+from main import db, create_app
 from models.user_icon import UserIcon
-from marshmallow.validate import Length
 from schemas.user_icon_schema import user_icon_schema, user_icons_schema
 
 # Default route for all user icon requests
@@ -14,16 +14,3 @@ def get_user_icons(user_activity_id):
     user_icons = db.session.query(UserIcon).with_entities(UserIcon.user_icon_url).filter(UserIcon.user_activity_id == user_activity_id)
     result = user_icons_schema.dump(user_icons)
     return jsonify(result)
-
-
-
-@user_icon.route('/create', methods=['POST'])
-def new_icon():
-    icon_fields = user_icon_schema.load(request.json)
-    icon = UserIcon(
-        user_icon_url = icon_fields ['user_icon_url'],
-    )
-
-    db.session.add(icon)
-    db.session.commit()
-    return jsonify((user_icon_schema).dump(icon))
--- main.py
+++ main.py
@@ -1,26 +1,24 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-from flask_cors import CORS, cross_origin
-
-# from datetime import timedelta
+from flask_cors import CORS
 from flask_marshmallow import Marshmallow
-# from marshmallow.validate import Length
-# import psycopg2
-# import config
 from flask_bcrypt import Bcrypt
-# from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+
 
 
 db = SQLAlchemy()
 ma = Marshmallow()
 bcrypt = Bcrypt()
 jwt = JWTManager()
-cors = CORS()
+
 
 def create_app():
 
     app = Flask(__name__)
+
+    CORS(app)
 
     app.config.from_object("config.app_config")
 
@@ -32,7 +30,10 @@ def create_app():
     from commands import db_commands
     app.register_blueprint(db_commands)
 
+    
+
     from controllers import registerable_controllers
     for controller in registerable_controllers:
         app.register_blueprint(controller)
     return app
+
